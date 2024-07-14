b3c9d50657095ad7ce1e892cc8488d2023b3e239
Linus Schilpp <linus.schilpp@gmail.com>
2024-07-14 18:58:23 +0200

    integrate free-fly-mode into fp controller

--- Assets/Scripts/Player/FPSController.cs
+++ Assets/Scripts/Player/FPSController.cs
@@ -11,6 +11,8 @@ public class FPSController : MonoBehaviour
     public float gravity = 9.81f;
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
+    public bool freeFlyMode = false;
+    public float freeFlySpeedMultiplier = 4f;
 
     private CharacterController controller;
     private Vector3 playerVelocity;
@@ -27,34 +29,47 @@ public class FPSController : MonoBehaviour
 
     private void Update()
     {
-        isGrounded = controller.isGrounded;
+        UpdateMovement();
+        UpdateCameraRotation();
+    }
 
-        if (isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
+    private void UpdateMovement() {
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        controller.Move(move * movementSpeed * Time.deltaTime);
 
-        if (Input.GetButton("Jump") && isGrounded)
-        {
-            playerVelocity.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+        if (freeFlyMode) {
+            if (Input.GetKey(KeyCode.Space)) {
+				controller.Move(Vector3.up * movementSpeed * freeFlySpeedMultiplier * Time.deltaTime);
+			}else if (Input.GetKey(KeyCode.LeftShift)) {
+				controller.Move(Vector3.down * movementSpeed * freeFlySpeedMultiplier * Time.deltaTime);
+			}
+            move *= freeFlySpeedMultiplier;
         }
+        else {
+            // Handle jumping
+            if (Input.GetButton("Jump") && controller.isGrounded)
+            {
+                playerVelocity.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
 
-        // Camera rotation
+            // Apply gravity
+            playerVelocity.y -= gravity * Time.deltaTime;
+
+            if (controller.isGrounded && playerVelocity.y < 0)
+            {
+                // Reset velocity
+                playerVelocity.y = 0f;
+            }
+        }
+        controller.Move(move * movementSpeed * Time.deltaTime + playerVelocity * Time.deltaTime);
+    }
+
+    private void UpdateCameraRotation() {
         rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
         rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
         playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-
-        // Apply gravity
-        playerVelocity.y -= gravity * Time.deltaTime;
-
-
-        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -20,43 +20,6 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
-		if (freeFlyMode) {
-			if (Input.GetKey(KeyCode.W)) {
-				playerGO.transform.position = new Vector3(
-					playerGO.transform.position.x + playerGO.transform.forward.x * moveSpeed,
-					playerGO.transform.position.y,
-					playerGO.transform.position.z + playerGO.transform.forward.z * moveSpeed);
-			}
-			if (Input.GetKey(KeyCode.A)) {
-				playerGO.transform.position = new Vector3(
-					playerGO.transform.position.x - playerGO.transform.right.x * moveSpeed,
-					playerGO.transform.position.y,
-					playerGO.transform.position.z - playerGO.transform.right.z * moveSpeed);
-			}
-			if (Input.GetKey(KeyCode.S)) {
-				playerGO.transform.position = new Vector3(
-					playerGO.transform.position.x - playerGO.transform.forward.x * moveSpeed,
-					playerGO.transform.position.y,
-					playerGO.transform.position.z - playerGO.transform.forward.z * moveSpeed);
-			}
-			if (Input.GetKey(KeyCode.D)) {
-				playerGO.transform.position = new Vector3(
-					playerGO.transform.position.x + playerGO.transform.right.x * moveSpeed,
-					playerGO.transform.position.y,
-					playerGO.transform.position.z + playerGO.transform.right.z * moveSpeed);
-			}
-			if (Input.GetKey(KeyCode.Space)) {
-				playerGO.transform.position = new Vector3(
-					playerGO.transform.position.x,
-					playerGO.transform.position.y + playerGO.transform.up.y * moveSpeed,
-					playerGO.transform.position.z);
-			}else if (Input.GetKey(KeyCode.LeftShift)) {
-				playerGO.transform.position = new Vector3(
-					playerGO.transform.position.x,
-					playerGO.transform.position.y - playerGO.transform.up.y * moveSpeed,
-					playerGO.transform.position.z);
-			}
-		}
 	}
 
 	public static void Spawn() {
