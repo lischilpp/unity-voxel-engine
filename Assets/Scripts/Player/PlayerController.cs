ef861284d5727aa08bedcef355eb996d66a5663b
Linus Schilpp <linus.schilpp@gmail.com>
2024-07-14 19:22:04 +0200

    cleanup player controller class

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,25 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
-
-	public bool freeFlyMode;
-	public float moveSpeed = 0.1f;
-
 	public static GameObject playerGO;
 	
 	void Start () {
 		playerGO = GameObject.Find("Player");
-		Debug.Log(playerGO);
 		playerGO.SetActive(false);
 	}
-
 	
-	// Update is called once per frame
-	void Update () {
-		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-	}
-
 	public static void Spawn() {
 		playerGO.SetActive(true);
 		PlayerOptions.playerSpawned = true;
